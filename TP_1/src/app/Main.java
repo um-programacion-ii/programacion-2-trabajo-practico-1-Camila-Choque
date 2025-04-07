52293a4d40eb0d93f5f6a8c668c66b98a83cbc12
Camila-Choque <m.choque@alumno.um.edu.ar>
2025-04-07 14:54:12 -0300

    arraylist

--- TP_1/src/app/Main.java
+++ TP_1/src/app/Main.java
@@ -4,62 +4,21 @@ import Vehiculos.Vehiculo;
 import Vehiculos.Camion;
 import Vehiculos.Auto;
 
+import java.util.ArrayList;
 
 
 public class Main {
     public static void main(String[] args) {
-        DatosVehiculo(null, "Toyota", 2001, 98);
-        DatosVehiculo ("AM15258", "Ford", 1800, 77);
-        DatosVehiculo("AE1478", "Peugeot", 2011, -50);
-        DatosVehiculo ("AA1489", "Kia", 2024, 30);
-        DatosVehiculo ("AB4589", "Mercedes", 2025, 80);
-        DatosCamion("AA124","Volvo",2020,15000,true);
-        DatosCamion(null,"CAT",2018,15000,true);
-        DatosAuto("AH1234","Mazda",2024,500,5);
-        DatosAuto("AC7894","Toyota",1800,200,2);
+
+        ArrayList<Vehiculo> listavehiculos = new ArrayList<>();
 
 
-    }
+        listavehiculos.add(new Vehiculo("AA1489", "Kia", 2024, 30));
+        listavehiculos.add(new Vehiculo("AB4589", "Mercedes", 2025, 80));
+        listavehiculos.add(new Camion("AA124", "Volvo", 2020, 15000, true));
+        listavehiculos.add(new Auto("AH1234", "Mazda", 2024, 500, 5));
 
-    //IMPRIME VEHICULOS Y VEHICULOS CON ERRORES
-    public static void  DatosVehiculo(String patente, String marca, int anio, double capacidadCargaKg) {
-        try {
-            Vehiculo v = new Vehiculo(patente, marca, anio, capacidadCargaKg);
-            System.out.println("INFORMACION DEL VEHICULO");
-            VehiculoPrinter.mostrarInformacion(v);
-            System.out.println("                             ");
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-            System.out.println("                             ");
-        }
 
-    }
-    //IMPRIME CAMIONES Y CAMIONES CON ERRORES
-    public static void DatosCamion(String patente, String marca, int anio, double capacidadCargaKg, boolean tieneAcoplado) {
-        try {
-            Camion c = new Camion(patente, marca, anio, capacidadCargaKg, tieneAcoplado);
-            System.out.println("INFORMACION DEL CAMION");
-            VehiculoPrinter.mostrarInformacion(c);
-            System.out.println("                             ");
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-            System.out.println("                             ");
-        }
-    }
-    //IMPRIME AUTOS Y AUTOS CON ERRORES
-    public static void DatosAuto(String patente, String marca, int anio, double capacidadCargaKg, int cantidadPasajeros) {
-        try {
-            Auto a = new Auto(patente, marca, anio, capacidadCargaKg, cantidadPasajeros);
-            System.out.println("INFORMACION DEL AUTO");
-            VehiculoPrinter.mostrarInformacion(a);
-            System.out.println("                             ");
-        }
-        catch (IllegalArgumentException e) {
-            System.out.println("Error: " + e.getMessage());
-            System.out.println("                             ");
-        }
 
     }
 }
